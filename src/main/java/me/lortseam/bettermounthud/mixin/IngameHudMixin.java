c39f4bdcd0509204296564508773c4cf0ed22b9f
Lortseam <lortseam@gmail.com>
2021-06-20 22:56:07 +0200

    Fix air bar position (#2)

--- src/main/java/me/lortseam/bettermounthud/mixin/IngameHudMixin.java
+++ src/main/java/me/lortseam/bettermounthud/mixin/IngameHudMixin.java
@@ -15,13 +15,21 @@ public abstract class IngameHudMixin {
     @Shadow @Final private MinecraftClient client;
 
     @ModifyConstant(method = "renderMountHealth", constant = @Constant(intValue = 39))
-    private int bettermounthud$moveMountHealthUp(int original) {
-        return original + 10;
+    private int bettermounthud$moveMountHealthUp(int yOffset) {
+        return yOffset + 10;
     }
 
     @Redirect(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;getHeartCount(Lnet/minecraft/entity/LivingEntity;)I"))
     private int bettermounthud$alwaysRenderFood(InGameHud inGameHud, LivingEntity entity) {
         return 0;
+    }
+
+    @ModifyVariable(method = "renderStatusBars", at = @At(value = "STORE", ordinal = 1), ordinal = 10)
+    private int bettermounthud$moveAirUp(int y) {
+        if (client.player.hasJumpingMount()) {
+            y -= 10;
+        }
+        return y;
     }
 
     @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;hasJumpingMount()Z"))
