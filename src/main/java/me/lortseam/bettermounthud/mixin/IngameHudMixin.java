e969c404f15a40122034eb99ac89cf22e4d85e8b
Lortseam <lortseam@gmail.com>
2022-03-20 18:21:04 +0100

    Fix code

--- src/main/java/me/lortseam/bettermounthud/mixin/IngameHudMixin.java
+++ src/main/java/me/lortseam/bettermounthud/mixin/IngameHudMixin.java
@@ -49,7 +49,7 @@ public abstract class IngameHudMixin {
     @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;hasJumpingMount()Z"))
     private boolean bettermounthud$switchBar(ClientPlayerEntity player) {
         if (!client.interactionManager.hasExperienceBar()) return player.hasJumpingMount();
-        return player.hasJumpingMount() && client.options.keyJump.isPressed() || player.getMountJumpStrength() > 0;
+        return player.hasJumpingMount() && client.options.jumpKey.isPressed() || player.getMountJumpStrength() > 0;
     }
 
 }
